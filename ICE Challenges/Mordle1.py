8f9f60c7568aa85ae047a586c7e7d83fe81265a7
TheaFisk <theafisk@gmail.com>
2025-03-04 13:28:52 -0700

    Update W03 assignment

--- ICE Challenges/Mordle1.py
+++ ICE Challenges/Mordle1.py
@@ -1,11 +1,28 @@
 
 # Variable is determined
 
-word = "spaghetti"
+word = "ocean"
+guess = " "
+attempts = 0
+#game begins
+print("  ")
+print("Welcome to the Mordle (TM) Guessing Game!")
+print("The word has 5 letters!")
 
 # loop to test the word
-
-while /== "spaghetti":
-    print("Your hint is:")
-
+while guess != word:
+    attempts = attempts + 1
+    print("   ")
+    print("Your hint is: _____")
+    guess = input("What is your Guess? ")
+    if len(guess) != 5:
+        print("  ")
+        print("That's not a 5 letter word!")
+    if guess != word:
+        print("Incorrect! Wrong word.")
+    
+    
 # Found the word!
+print("   ")
+print("Congratulations! You found the word!")
+print(f"It took you", attempts, "attempts.")
